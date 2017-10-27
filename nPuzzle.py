3ba102068278bc40c30bb4f03d164569820f5f42
Audrey Der <ader003@ucr.edu>
2017-10-27 10:55:02 -0700

    added move function

--- nPuzzle.py
+++ nPuzzle.py
@@ -1,6 +1,5 @@
-#import heapq
-
-#the different choices in puzzles offered/test cases
+# import heapq
+# the different choices in puzzles offered/test cases
 trivial = [[1, 2, 3],
            [4, 5, 6],
            [7, 8, 0]]
@@ -26,54 +25,103 @@ goalState = [[1, 2, 3],
 
 
 def main():
-    puzzleMode = input("Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own."
+    puzzle_mode = input("Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own."
                        + '\n')
-    if puzzleMode == "1":
-        printPuzzle(defaultPuzzleMode())
+    if puzzle_mode == "1":
+        print_puzzle(default_puzzle_mode())
 
-    if puzzleMode == "2":
+    if puzzle_mode == "2":
         print("Enter your puzzle, using a zero to represent the blank. " +
               "Enter each row using a space or tab between each number. RET only when finished." + '\n')
         input(userPuzzle_string)
-        userPuzzle = [x for x in userPuzzle_string in userPuzzle_string]
-        print(userPuzzle) #TODO: delete; temporary for for testing purposes
-
-#        for i in range(0, 2):
-#            for j in range(0, 2):
-#                puzzleString = input(userPuzzle.at(i, j))
-#                userPuzzle = puzzleString.str()
+        user_puzzle = [x for x in userPuzzle_string in userPuzzle_string]
+        print(user_puzzle)  # TODO: delete; temporary for for testing purposes
     return
 
 
-
-def defaultPuzzleMode():
-    selectedDifficulty = input(
+def default_puzzle_mode():
+    selected_difficulty = input(
         "You wish to use a default puzzle. Please enter a desired difficulty on a scale from 0 to 5." + '\n')
-    if selectedDifficulty == "0":
+    if selected_difficulty == "0":
         print("Difficulty of 'Trivial' selected.")
         return trivial
-    if selectedDifficulty == "1":
+    if selected_difficulty == "1":
         print("Difficulty of 'Very Easy' selected.")
         return veryEasy
-    if selectedDifficulty == "2":
+    if selected_difficulty == "2":
         print("Difficulty of 'Easy' selected.")
         return easy
-    if selectedDifficulty == "3":
+    if selected_difficulty == "3":
         print("Difficulty of 'Doable' selected.")
         return doable
-    if selectedDifficulty == "4":
+    if selected_difficulty == "4":
         print("Difficulty of 'Oh Boy' selected.")
         return ohBoy
-    if selectedDifficulty == "5":
+    if selected_difficulty == "5":
         print("Difficulty of 'Impossible' selected.")
         return impossible
 
 
-def printPuzzle(puzzle):
+def print_puzzle(puzzle):
     for i in range(0, 3):
         print(puzzle[i])
 
+
+def uniform_cost_search():  # basically BFS, keeping track of how many L's deep you've expanded
+    nodes_expanded = 0  # he wants this data
+    cost = 0
+
+    return cost, nodes_expanded
+
+
+def misplaced_tile_heuristic():
+    nodes_expanded = 0
+    cost = 0
+
+    return cost, nodes_expanded
+
+
+def manhattan_distance_heuristic():
+    nodes_expanded = 0
+    cost = 0
+
+    return cost, nodes_expanded
+
+
+def move_zero(direction, zero_position):  # TODO: REWRITE; CURRENTLY, MORE LINES THAN NECESSARY
+    if direction == "up":
+        position_holder = zero_position
+        # look at the index above
+        above_position = 0 # find it
+        zero_position = above_position
+        above_position = position_holder
+        return zero_position, above_position
+
+    if direction == "down":
+        position_holder = zero_position
+        # look at the index above
+        below_position = 0  # find it
+        zero_position = below_position
+        below_position = position_holder
+        return zero_position, below_position
+
+    if direction == "right":
+        position_holder = zero_position
+        # look at the index above
+        right_position = 0  # find it
+        zero_position = right_position
+        right_position = position_holder
+        return zero_position, right_position
+
+    if direction == "left":
+        position_holder = zero_position
+        # look at the index above
+        left_position = 0  # find it
+        zero_position = left_position
+        left_position = position_holder
+        return zero_position, left_position
+
+
+
 if __name__ == '__main__':
     main()
-
-
