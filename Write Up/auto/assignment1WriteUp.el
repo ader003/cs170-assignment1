3e0afc0c82a3e9e156161ea11229ac3d7a34adda
Audrey Der <ader003@ucr.edu>
2017-11-03 01:26:23 -0700

    Proofread and finished write up

--- Write Up/auto/assignment1WriteUp.el
+++ Write Up/auto/assignment1WriteUp.el
@@ -5,6 +5,13 @@
                      '(("article" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english") ("inputenc" "utf8x") ("fontenc" "T1") ("geometry" "a4paper" "top=3cm" "bottom=2cm" "left=3cm" "right=3cm" "marginparwidth=1.75cm") ("todonotes" "colorinlistoftodos") ("hyperref" "colorlinks=true" "allcolors=blue")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -12,17 +19,14 @@
     "babel"
     "inputenc"
     "fontenc"
+    "multicol"
     "geometry"
     "amsmath"
     "graphicx"
     "todonotes"
     "hyperref")
    (LaTeX-add-labels
-    "fig:Number of Nodes Expanded"
-    "fig:Maximum Queue Size"
-    "fig:frog"
-    "tab:widgets")
-   (LaTeX-add-bibliographies
-    "sample"))
+    "Figure 1"
+    "Figure 2"))
  :latex)
 
